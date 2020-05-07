6310a306c5c1363cd6a8ecca1f1cb3b00fc27366
buanzo <buanzo@buanzo.com.ar>
2020-05-06 22:11:47 -0300

    Repositioned class doc to match __doc__ functionality

--- humetools.py
+++ humetools.py
@@ -2,10 +2,10 @@
 import sys
 import argparse
 
-""" This class allows to work on getting your Argparse object
-    ready even if nothing useful happens when used.
 
-    Save this git on some file then import the class.
+class NotImplementedAction(argparse.Action):
+    """ This class allows to work on getting your Argparse object
+    ready even if nothing useful happens when used.
 
     Usage:
     Just set action=NotImplementedAction when calling add_argument, like this:
@@ -17,10 +17,7 @@ import argparse
                         action=NotImplementedAction)
 
     GIST URL: https://gist.github.com/buanzo/2a004348340ef79b0139ab38f719cf1e
-"""
-
-
-class NotImplementedAction(argparse.Action):
+    """
     def __call__(self, parser, namespace, values, option_string=None):
         msg = 'Argument "{}" still not implemented.'.format(option_string)
         sys.exit(msg)

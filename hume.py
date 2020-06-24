f9574419b1dd9fcc6e6c263576431ef425fda151
buanzo <buanzo@buanzo.com.ar>
2020-06-24 10:53:03 -0300

    updated requirements.txt and bumped version to 1.2.5

--- hume.py
+++ hume.py
@@ -10,7 +10,7 @@ from pprint import pprint
 from datetime import datetime
 from humetools import NotImplementedAction
 
-__version__ = '1.2.3'
+__version__ = '1.2.5'
 
 class Hume():
     def __init__(self, args):
